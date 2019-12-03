7daf064cd906e9559c7555af1ca1ac9c59a673b5
erik.araujo <erik.araujo@TI002844>
2019-12-03 08:17:31 -0400

    Initial Commit

---
+++ JobPageDevFest/Controllers/DevController.cs
@@ -0,0 +1,52 @@
+﻿using JobPageDevFest.Models;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPageDevFest.Controllers
+{
+    public class DevController : Controller
+    {
+        private string Erro { get; set; }
+        // GET: Dev
+        public ActionResult Index()
+        {
+            Erro = Erro ?? "false";
+            ViewBag.Erro = Erro;
+            return View();
+        }
+
+        //public ActionResult Create(DadosCandidato dados)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        HttpPostedFileBase arquivo = Request.Files?[0];
+
+        //        if (arquivo != null)
+        //        {
+        //            var curriculo = new Attachment(arquivo.InputStream, $"Curriculo Candidato {dados.Nome}.{arquivo.FileName.Split('.')[arquivo.FileName.Split('.').Length - 1]}");
+
+        //            var dadosEmail = new DadosEmail()
+        //            {
+        //                Assunto = $"Currículo {dados.Nome}",
+        //                Mensagem = $"E-mail do candidato: {dados.Email}",
+        //                ListaAnexo = new List<Attachment>() { curriculo },
+        //                Titulo = "VAGA TI - Envio de currículo",
+        //                ListaDestinatarios = new List<string> { "ti@redeflex.com.br" }
+        //            };
+
+                                        
+        //            new Email().EnviaEmail(TipoEmail.Integracao, "Interface cadastro de currículo", dadosEmail);
+        //            Erro = "false";
+        //        }
+        //        else
+        //        {
+        //            Erro = "true";
+        //        }
+        //    }
+
+        //    return Index();
+        //}
+    }
+}
---
+++ JobPageDevFest/Models/DadosCandidato.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace JobPageDevFest.Models
+{
+    public class DadosCandidato
+    {
+        [Required(ErrorMessage = " Campo nome não pode ser nulo")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = " Campo email não pode ser nulo")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = " Campo Currículo não pode ser nulo")]
+        public HttpPostedFileBase Curriculo { get; set; }
+
+    }
+}
